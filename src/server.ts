8983258b45fe90cb8d71da19fa12d99368c9e52a
luciane003 <lucianekellenpsarges@gmail.com>
2026-01-02 18:45:42 -0300

    Primeiro commit

---
+++ src/server.ts
@@ -0,0 +1,52 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+let anotacoes: {id: number; texto: string;}[] = [];
+
+app.get("/", (req, res) => {
+  res.send("API de anotações funcionando!");
+});
+
+app.get("/anotacoes", (req, res) => {
+  res.json(anotacoes);
+});
+
+app.post("/anotacoes", (req, res) => {
+  const {texto} = req.body;
+
+  if(!texto || texto.trim() ==="") {
+    return res.status(400).json({message: "O texto da anotação é obrigatório"});
+  }
+
+  const novaAnotacao = {
+    id: Date.now(), 
+    texto
+  };
+
+  anotacoes.push(novaAnotacao);
+
+  res.status(201).json(novaAnotacao);
+});
+
+
+app.delete("/anotacoes/:id", (req, res) => {
+  const id = Number(req.params.id);
+
+  const anotacaoExistente = anotacoes.some(nota => nota.id === id);
+
+  if(!anotacaoExistente) {
+    return res.status(404).json({message: "Anotação não encontrada."});
+  }
+
+  anotacoes = anotacoes.filter(nota => nota.id !== id);
+  
+  res.status(200).json({message: "Anotação deletada com sucesso!"});
+});
+
+app.listen(3000,  () => {
+    console.log('Servidor em execução em http://localhost:3000');
+});
